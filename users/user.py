8ef99f2a5740f1069a5b5ff6237d4334e3be5656
Balaji-0 <balaji3097@gmail.com>
2024-09-25 22:22:41 +0530

    users dockerfile and requirement.txt file is created and coded

---
+++ users/user.py
@@ -0,0 +1,16 @@
+import boto3
+import streamlit as st
+import os
+import uuid
+
+
+
+
+
+def main():
+    st.header("This is client Site for Chat with PDF demo using Bedrock, RAG etc")
+   
+
+
+if __name__ == "__main__":
+    main()
